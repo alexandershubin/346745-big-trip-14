6e821292233692bb634b97e77b55b2e6e429a872
alexandershubin <shubin8@mail.ru>
2021-04-07 19:42:43 +0300

    module2-task1 - Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import {createMenuTemplate} from './view/menu';
 import {createPointTripTemplate} from './view/point-trip';
 import {createSortTemplate} from './view/sort';
 import {createInfoTripTemplate} from './view/trip-info';
+import {generatePoint} from './mock/point';
 
 const TASK_COUNT = 3;
+const points = new Array(TASK_COUNT).fill().map(generatePoint);
 
-const render = (container, template, place= 'beforeend') => {
+const render = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
 
@@ -21,9 +23,9 @@ render(siteHeaderElement, createInfoTripTemplate(), 'afterbegin');
 render(siteNavElement, createMenuTemplate());
 render(siteFilterElement, createFiltersTemplate());
 render(siteMainElement, createSortTemplate());
-render(siteMainElement, createFormEditTemplate());
-// render(siteMainElement, createFormCreationTemplate()) // форма редактирование пока скрыта
+render(siteMainElement, createFormEditTemplate(points[0]));
+// render(siteMainElement, createFormCreationTemplate(points[0])); // форма создания пока скрыта
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  render(siteMainElement, createPointTripTemplate());
+  render(siteMainElement, createPointTripTemplate(points[i]));
 }
